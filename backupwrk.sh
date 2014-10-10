e438a9c8b2f94d3b95cc284db6e6c1ec95cf542c
Nemo112 <nemo@nemor.cz>
2014-10-10 18:49:37 +0200

    Corrections

--- backupwrk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-p=$PWD;
-cd ..;
-tar cvf ./contBlock.tar ./contBlock;
-gzip -f ./contBlock.tar;
-scp ./contBlock.tar.gz nemo@nemor.cz:/raid/home/nemo/00_imp_data_rep_nemo/SKOLA2/BI-ADS;
-cd $p;
